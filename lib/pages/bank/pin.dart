bfb70fc45844225c859d19abe58ec908a0cbb18d
Dagim Teshome <140588173+dagm321@users.noreply.github.com>
2026-04-06 15:06:59 -0700

    Update pin.dart

--- lib/pages/bank/pin.dart
+++ lib/pages/bank/pin.dart
@@ -56,7 +56,7 @@ class Pin {
                         const SizedBox(height: 10),
                         const Text(
                           'Enter PIN',
-                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
                         ),
                         const SizedBox(height: 8),
                         Text.rich(
@@ -64,11 +64,11 @@ class Pin {
                             children: [
                               TextSpan(
                                 text: '${NumberFormat('#,###').format(phoneNumberinpController.bankAmount.value)}.00 ',
-                                style: const TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+                                style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
                               ),
                               TextSpan(
                                 text: 'ETB',
-                                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                                style: const TextStyle(fontSize: 15),
                               ),
                             ],
                           ),
@@ -88,7 +88,7 @@ class Pin {
                               child: Text(
                                 index < pin.length ? '●' : '',
                                 style: const TextStyle(
-                                  fontSize: 20,
+                                  fontSize: 16,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
@@ -178,10 +178,10 @@ class NumberPad extends StatelessWidget {
             ),
             alignment: Alignment.center,
             child: key == '←'
-                ? const Icon(Icons.backspace_outlined, color: Color.fromARGB(255, 87, 86, 86), size: 20,)
+                ? const Icon(Icons.backspace_outlined, color: Color.fromARGB(255, 87, 86, 86), size: 18,)
                 : Text(
                     key,
-                    style: const TextStyle(fontSize: 24),
+                    style: const TextStyle(fontSize: 21),
                   ),
           ),
         );
