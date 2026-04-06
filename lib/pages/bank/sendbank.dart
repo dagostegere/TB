8b273642bd5cd8b28948009b290edf7e1dd65f0a
Dagim Teshome <140588173+dagm321@users.noreply.github.com>
2026-04-06 15:39:51 -0700

    Increase font size for various text elements

--- lib/pages/bank/sendbank.dart
+++ lib/pages/bank/sendbank.dart
@@ -452,7 +452,7 @@ class _SendbankState extends State<Sendbank> {
                                               child:
                                                 Column(
                                                   children: [
-                                                    Text("Transfer to Bank", style: TextStyle(fontSize: 17, color: Colors.black87)),
+                                                    Text("Transfer to Bank", style: TextStyle(fontSize: 18, color: Colors.black87)),
                                                     Container(
                                                       padding: EdgeInsets.all(8),
                                                       child: Obx(() => 
@@ -490,8 +490,8 @@ class _SendbankState extends State<Sendbank> {
                                                               child: Row(
                                                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                                                 children: [
-                                                                  Text("Original Amount", style: TextStyle(fontSize: 15, color: Colors.grey),),
-                                                                  Text("${phoneNumberinpController.originalBankAmount}.00ETB", style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.w600))
+                                                                  Text("Original Amount", style: TextStyle(fontSize: 16, color: Colors.grey),),
+                                                                  Text("${phoneNumberinpController.originalBankAmount}.00ETB", style: TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.w600))
                                                                 ],
                                                               ),
                                                             ),
@@ -501,13 +501,13 @@ class _SendbankState extends State<Sendbank> {
                                                               child: Row(
                                                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                                                 children: [
-                                                                  Text("Service fee", style: TextStyle(fontSize: 15, color: Colors.grey),),
+                                                                  Text("Service fee", style: TextStyle(fontSize: 16, color: Colors.grey),),
                                                                   Text(phoneNumberinpController.bankAmount < 101 ? "1.00" : 
                                                                     phoneNumberinpController.bankAmount > 99 && phoneNumberinpController.bankAmount < 500 ? "3.00" :
                                                                     phoneNumberinpController.bankAmount >= 500 && phoneNumberinpController.bankAmount < 1000 ? "6.00" :
                                                                     phoneNumberinpController.bankAmount >= 1000 && phoneNumberinpController.bankAmount < 5000 ? "7.00" :
                                                                     "9.00", 
-                                                                   style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.w600))
+                                                                   style: TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.w600))
                                                                 ],
                                                               ),
                                                             ),
@@ -534,7 +534,7 @@ class _SendbankState extends State<Sendbank> {
                                                               alignment: Alignment.centerLeft,
                                                               padding: EdgeInsets.all(10),
                                                               margin: EdgeInsets.symmetric(horizontal: 5),
-                                                              child: Text("Payment Method", style: TextStyle(fontSize: 15, color: Colors.grey),),
+                                                              child: Text("Payment Method", style: TextStyle(fontSize: 16, color: Colors.grey),),
                                                             ),
                                                             Container(
                                                               width: double.infinity,
